6e4ed73f980b867af1f24316c9876e5ab171fbd8
Derrick Boega Nyabuto <boegaderrick@gmail.com>
2022-12-15 19:33:47 +0000

    monty push/pall

---
+++ monty.c
@@ -0,0 +1,120 @@
+#include "monty.h"
+
+vars_t vars = {NULL, NULL, NULL};
+
+/**
+ * free_stack - frees memory held by stack
+ * @stack: pointer to top of stack
+ */
+void free_stack(stack_t *stack)
+{
+	stack_t *temp;
+
+	while (stack)
+	{
+		temp = stack;
+		stack = stack->next;
+		free(temp);
+	}
+	stack = NULL;
+}
+
+/**
+ * _op - scans for & executes function matching passed opcode
+ * @stack: double pointer to top of stack
+ * @line_number: line position of opcode in file
+ */
+void _op(stack_t **stack, unsigned int line_number)
+{
+	int i;
+	bool valid = false;
+
+	instruction_t instructions[] = {
+		{"push", push},
+		{"pall", pall}
+	};
+
+	for (i = 0; i < 2; i++)
+	{
+		if (strcmp(instructions[i].opcode, vars.cmd) == 0)
+		{
+			valid = true;
+			instructions[i].f(stack, line_number);
+			break;
+		}
+	}
+	if (!valid)
+	{
+		fprintf(stderr, "L%d: unknown instruction %s\n", line_number, vars.cmd);
+		free(vars.c_line);
+		free_stack(*stack);
+		exit(EXIT_FAILURE);
+	}
+}
+
+/**
+ * error_handler - handles errors encoutered at program start
+ * @err_code: error reference code
+ * @ref: reference of what caused the error
+ *			- | - | -
+ * <err_code> reference:
+ *	0 = wrong number of args
+ *	1 = file open failed
+ */
+void error_handler(int err_code, char *ref)
+{
+	char error[50] = "";
+
+	switch (err_code)
+	{
+		case 0:
+			write(STDERR_FILENO, "USAGE: monty file\n", 18);
+			break;
+
+		case 1:
+			strcat(strcat(strcat(error, "Error: Can't open the file "), ref), "\n");
+			write(STDERR_FILENO, error, strlen(error));
+			break;
+	}
+	free(vars.c_line);
+	exit(EXIT_FAILURE);
+}
+
+
+/**
+ * main - this is a function
+ * @argc: number of arguments passed to program
+ * @argv: 2D array of strings containing arguments passed
+ * Return: 0
+ *
+ * Description: program manipulates a stack data structure
+ */
+int main(int argc, char **argv)
+{
+	int fd, line_number = 1;
+	size_t c_size = 1;
+	char *path, *error, *c_line = NULL, *delim = " \t\n";
+	FILE *file;
+	stack_t *stack = NULL;
+
+	if (argc != 2)
+		error_handler(0, path);
+	path = argv[1];
+	file = fopen(path, "r");
+	if (!file)
+		error_handler(1, path);
+	while (getline(&c_line, &c_size, file) != -1)
+	{
+		vars.c_line = c_line;
+		vars.cmd = strtok(c_line, delim);
+		vars.elem = strtok(NULL, delim);
+		if (vars.cmd && vars.cmd[0] != '#')
+			_op(&stack, line_number);
+		free(c_line);
+		c_line = NULL;
+		line_number++;
+	}
+	fclose(file);
+	free_stack(stack);
+	return (0);
+}
---
+++ ops.c
@@ -0,0 +1,73 @@
+#include "monty.h"
+
+/**
+ * _isdigit - custom isdigit function to check whether input is valid number
+ * Return: true if valid, else false
+ */
+bool _isdigit(void)
+{
+	bool digit = true;
+	int i;
+
+	for (i = 0; i < strlen(vars.elem); i++)
+	{
+		if (!isdigit(vars.elem[i]))
+			if (i == 0 && vars.elem[i] == '-' || vars.elem[i] == '+')
+				continue;
+			else
+				digit = false;
+	}
+	return (digit);
+}
+
+/**
+ * push - adds element to top of stack
+ * @stack: double pointer to top of stack
+ * @line_number: line in file where command was extracted
+ */
+void push(stack_t **stack, unsigned int line_number)
+{
+	stack_t *new, *temp = *stack;
+
+	if (!vars.elem || !_isdigit())
+	{
+		fprintf(stderr, "L%d: usage: push integer\n", line_number);
+		free(vars.c_line);
+		free_stack(*stack);
+		exit(EXIT_FAILURE);
+	}
+	new = malloc(sizeof(stack_t));
+	if (!new)
+	{
+		fprintf(stderr, "Error: malloc failed\n");
+		free(vars.c_line);
+		free_stack(*stack);
+		exit(EXIT_FAILURE);
+	}
+	new->n = atoi(vars.elem);
+	new->prev = NULL;
+	if (!temp)
+		new->next = NULL;
+	else
+	{
+		new->next = temp;
+		temp->prev = new;
+	}
+	*stack = new;
+}
+
+/**
+ * pall - prints all elements of a stack
+ * @stack: double pointer to top of stack
+ * @line_number: line in file where command was extracted
+ */
+void pall(stack_t **stack, unsigned int line_number)
+{
+	stack_t *temp = *stack;
+
+	while (temp)
+	{
+		printf("%d\n", temp->n);
+		temp = temp->next;
+	}
+}
