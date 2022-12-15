320a59d1f1db0bd0f6c5206e2ce7bfc46a559945
Derrick Boega Nyabuto <boegaderrick@gmail.com>
2022-12-15 23:26:27 +0000

    stack pchar ascii

--- monty.c
+++ monty.c
@@ -38,10 +38,11 @@ void _op(stack_t **stack, unsigned int line_number)
 		{"sub", sub},
 		{"div", division},
 		{"mul", mul},
-		{"mod", mod}
+		{"mod", mod},
+		{"pchar", pchar}
 	};
 
-	for (i = 0; i < 9; i++)
+	for (i = 0; i < 10; i++)
 	{
 		if (strcmp(instructions[i].opcode, vars.cmd) == 0)
 		{
---
+++ ops2.c
@@ -0,0 +1,31 @@
+#include "monty.h"
+
+/**
+ * pchar - prints char represented by top element in ascii
+ * @stack: double pointer to top of the stack
+ * @line_number: line position in file where command was extracted
+ */
+void pchar(stack_t **stack, unsigned int line_number)
+{
+	stack_t *temp = *stack;
+
+	if (temp)
+	{
+		if (temp->n > -1 && temp->n < 128)
+			printf("%c\n", temp->n);
+		else
+		{
+			fprintf(stderr, "L%d: can't pchar, value out of range\n", line_number);
+			free(vars.c_line);
+			free_stack(*stack);
+			exit(EXIT_FAILURE);
+		}
+	}
+	else
+	{
+		fprintf(stderr, "L%d: can't pchar, stack empty\n", line_number);
+		free(vars.c_line);
+		free_stack(*stack);
+		exit(EXIT_FAILURE);
+	}
+}
